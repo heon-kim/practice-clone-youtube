6124aac97d4ee23184df0e70fcd96b3932edfd07
heon-kim <vndtka123@gmail.com>
2022-03-19 04:52:30 +0900

    Add Router.js & Controller.js clear

---
+++ src/controllers/videoController.js
@@ -0,0 +1,4 @@
+export const trending = (req, res) => res.send("Trending Video");
+
+export const watch = (req, res) => res.send("Watch Video");
+export const edit = (req, res) => res.send("Edit Video");
--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
-
 app.use(morgan("dev"));
-app.get("/", (request, response) => {
-  return response.send("this is response");
-});
 
-app.get("/protected", (req, res) => {
-  return res.send("welcome to the private lounge");
-});
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () =>
   console.log(`✅ server listening on port http://localhost:${PORT} 👍`)
