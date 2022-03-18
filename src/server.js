91bc6da171f0495c56f074aca232537ca1111bda
heon-kim <vndtka123@gmail.com>
2022-03-18 21:44:45 +0900

    app. listen, get, use

--- src/server.js
+++ src/server.js
@@ -3,6 +3,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+app.use((request, response, next) => {
+  console.log("this is middleware");
+  console.log(`we are going to: ${request.url}`);
+  next();
+});
+
+app.get("/", (request, response) => {
+  return response.send("this is response");
+});
+
 app.listen(PORT, () =>
   console.log(`✅ server listening on port http://localhost:${PORT} 👍`)
 );
