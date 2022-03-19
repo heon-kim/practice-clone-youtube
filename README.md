60d042d799d66b8bd6b789a36e4678b64e96883c
heon-kim <vndtka123@gmail.com>
2022-03-19 13:35:36 +0900

    Edit routers, Add URLparameters

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 /login -> Login
 /search -> Search
 
-/users/edit -> Edit user
-/users/delete -> Delete user
+/users/:id -> See user
+/users/logout -> Log out
+/users/edit -> Edit my profile
+/users/remove -> Delete my profile
 
-/videos/watch -> Watch Video
-/videos/edit -> Edit Video
-/videos/delete -> Delete Video
-/videos/comments -> Comment On a Video
-/videos/comments/delete -> Delete a Comment of a Video
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/remove -> remove Video
+/videos/upload -> Upload Video
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,8 @@
-export const trending = (req, res) => res.send("Trending Video");
-
-export const watch = (req, res) => res.send("Watch Video");
+export const trending = (req, res) => res.send("Homepage Trending Video");
+export const search = (req, res) => res.send("search");
+export const upload = (req, res) => res.send("upload Video");
+export const see = (req, res) => {
+  return res.send(`Watch Vieo #${req.params.id}`);
+};
 export const edit = (req, res) => res.send("Edit Video");
+export const remove = (req, res) => res.send("remove Video");
