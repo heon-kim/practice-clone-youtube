3356d18548a6a5093bab38512c61ffbf6f6c7b35
heon-kim <vndtka123@gmail.com>
2022-03-20 20:10:22 +0900

    Post edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,7 +33,15 @@ export const watch = (req, res) => {
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("upload Video");
-export const edit = (req, res) => res.send("edit");
-export const remove = (req, res) => res.send("remove Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  console.log(req.body);
+  return res.redirect(`/videos/${id}`);
+};
