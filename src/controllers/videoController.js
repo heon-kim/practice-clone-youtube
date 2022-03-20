f0c243f4c8f29b83613be305e0f220fc61e87e5f
heon-kim <vndtka123@gmail.com>
2022-03-20 17:52:13 +0900

    Add video pages

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,39 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove Video");
