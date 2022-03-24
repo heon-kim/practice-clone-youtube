e3f2c470b978df3732878b7ddf35e0f1bbaf6a05
heon-kim <vndtka123@gmail.com>
2022-03-25 00:41:04 +0900

    Upload to DB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
@@ -17,6 +14,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-app.listen(PORT, () =>
-  console.log(`✅ server listening on port http://localhost:${PORT} 👍`)
-);
+export default app;
