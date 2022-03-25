f6aa87c8c14b6ff35f6e13fa86f123da44fd8974
heon-kim <vndtka123@gmail.com>
2022-03-26 01:09:40 +0900

    Fix errors- await need async

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -22,18 +22,22 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  video.save();
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(`🚨 ${error}`);
+    return res.render("upload", { pageTitle: "Upload Video" });
+  }
 };
